638fa46168409230ff30789fb911bdc2f32ea224
bee <bernhard.spindler.75@gmail.com>
2020-10-22 05:18:10 +0200

    c++ operators

--- C_C++/SlidingTimeWindow/timebuffer.cpp
+++ C_C++/SlidingTimeWindow/timebuffer.cpp
@@ -1,43 +1,60 @@
+//----------------------------------------------------------------------------
 class Timespan
+//----------------------------------------------------------------------------
 {
-
 public:
 
-    long int ticks;
+    long int _ticks;
 
-    Timespan(long int duration)
-    {
-        this->ticks = duration;
+    Timespan(long int ticks) : _ticks(ticks) { }
+
+    Timespan operator+(const Timespan& other) const { return Timespan(_ticks + other._ticks); }
+    Timespan operator-(const Timespan& other) const { return Timespan(_ticks - other._ticks); }
+
+    Timespan& operator+=(const Timespan& other) 
+    { 
+        _ticks += other._ticks;
+        return *this;
     }
 
+    Timespan& operator-=(const Timespan& other)
+    {
+        _ticks -= other._ticks;
+        return *this;
+    }
+
+    /*
     void Add(Timespan other)
     {
-        ticks += other.ticks;
+        _ticks += other._ticks;
     }
     void Sub(Timespan other)
     {
-        ticks -= other.ticks;
+        _ticks -= other._ticks;
     }
+    */
 };
-
+//----------------------------------------------------------------------------
 class Timestamp
+//----------------------------------------------------------------------------
 {
 private:
-    long int ticks;
+    long int _ticks;
 
 public:
-    Timestamp(long int pointInTime) : ticks(pointInTime)
-    {
-    }
-    Timestamp() : ticks(0)
-    {
-    }
+    Timestamp(long int pointInTime) : _ticks(pointInTime) { }
+    Timestamp()                     : _ticks(0) { }
 
     bool after(const Timestamp other) const
     {
-        return (ticks - other.ticks) > 0;
+        return (_ticks - other._ticks) > 0;
     }
 
+    Timestamp operator+(Timestamp& other)    const { return Timestamp(_ticks + other._ticks); }
+    Timespan  operator-(Timestamp& other)    const { return Timespan (_ticks - other._ticks); }
+    Timestamp operator-(Timespan&  duration) const { return Timestamp(_ticks - duration._ticks); }
+
+    /*
     Timespan Sub(const Timestamp other) const
     {
         return Timespan(ticks - other.ticks);
@@ -45,13 +62,15 @@ public:
 
     Timestamp SubstractTimespan(Timespan span) const
     {
-        return Timestamp(ticks - span.ticks);
+        return Timestamp(ticks - span._ticks);
     }
+    */
 };
 
 #define MAX_ENTRIES 1024
-
+//----------------------------------------------------------------------------
 class TimeBuffer
+//----------------------------------------------------------------------------
 {
 public:
     TimeBuffer(long int range, long int timestamp, int startwith)
@@ -79,19 +98,19 @@ void TimeBuffer::set(const Timestamp timestamp, int onEvent, Timespan* onTicks, 
 {
     // check if full
 
-    const Timespan delta = timestamp.Sub(now);
+    const Timespan delta = timestamp - now;
 
     // check if delta >= 0
 
     if (f_active == onEvent) {
 
-        ticks[onEvent].Add(delta);
+        ticks[onEvent] += delta;
         now = timestamp;
 
     }
     else { // next entry
 
-        ticks[1 - onEvent].Add(delta);
+        ticks[1 - onEvent] += delta;
         frontIdx = (frontIdx + 1) % MAX_ENTRIES;
         ts[frontIdx] = now = timestamp;
         f_active = onEvent;
@@ -100,7 +119,7 @@ void TimeBuffer::set(const Timestamp timestamp, int onEvent, Timespan* onTicks, 
 
     // now the processing for the back
 
-    const Timestamp oldest_ts_inRange = timestamp.SubstractTimespan(range);   //timestamp -= range;
+    const Timestamp oldest_ts_inRange = timestamp - range;   //timestamp -= range;
     Timestamp back_ts = ts[backIdx];                                    //back_ts = _tb->ts[_tb->back];
     onEvent = b_active;
     
@@ -114,7 +133,7 @@ void TimeBuffer::set(const Timestamp timestamp, int onEvent, Timespan* onTicks, 
 
             if ( next_ts.after(oldest_ts_inRange) ) {                   //if (after(oldest, next_ts)) {
 
-                ticks[onEvent].Sub( next_ts.Sub(back_ts) );             //_tb->ticks[onEvent] -= next_ts - back_ts;
+                ticks[onEvent] -= next_ts - back_ts;             //_tb->ticks[onEvent] -= next_ts - back_ts;
                 
                 backIdx = (backIdx + 1) % MAX_ENTRIES;
                 onEvent = 1 - onEvent;
@@ -123,7 +142,7 @@ void TimeBuffer::set(const Timestamp timestamp, int onEvent, Timespan* onTicks, 
             }
             else {
 
-                ticks[onEvent].Sub( oldest_ts_inRange.Sub(back_ts) );   //_tb->ticks[onEvent] -= oldest - back_ts;
+                ticks[onEvent] -= oldest_ts_inRange - back_ts;   //_tb->ticks[onEvent] -= oldest - back_ts;
                 
                 ts[backIdx] = oldest_ts_inRange;
                 b_active = onEvent;
